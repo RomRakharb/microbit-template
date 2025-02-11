a00acfd7aa419dec86b6201bd21b9ab4ac4b7b05
Rom Rakharb <romr.dev@proton.me>
2025-02-11 17:34:51 +0000

    setup

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -11,8 +11,11 @@ sudo apt update
 sudo add-apt-repository -y ppa:maveonair/helix-editor
 sudo apt update
 sudo apt install helix
-cargo install bacon
-rustup component add rust-analyzer
+cargo init
+rustup component add rust-analyzer llvm-tools
+rustup target add thumbv7em-none-eabihf
+cargo install bacon cargo-binutils
+yes | curl --proto '=https' --tlsv1.2 -LsSf https://github.com/probe-rs/probe-rs/releases/latest/download/probe-rs-tools-installer.sh | sh
 echo
 echo "---------------------"
 echo "install script ends"
